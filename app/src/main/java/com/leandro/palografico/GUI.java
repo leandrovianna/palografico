d723d075391f321853d7daee6613a5cb92e1452f
leandrovianna <leandrovianna50@gmail.com>
2015-03-12 12:09:21 -0300

    Adicionando método de criar dialogo de loading na classe GUI

--- app/src/main/java/com/leandro/palografico/GUI.java
+++ app/src/main/java/com/leandro/palografico/GUI.java
@@ -1,15 +1,28 @@
 package com.leandro.palografico;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class GUI {
 
-	public static void showDialog(String title, String text, Context context) {
+	public static AlertDialog showDialog(String title, String text, Context context) {
 		AlertDialog.Builder dialogo = new AlertDialog.Builder(context);
 		dialogo.setTitle(title)
 			.setMessage(text)
-			.setPositiveButton("OK", null)
-			.show();
+			.setPositiveButton("OK", null);
+
+        return dialogo.show();
 	}
-	
+
+    public static AlertDialog showLoadingDialog(Context context) {
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+
+        dialog.setTitle("Carregando...");
+        dialog.setView(new ProgressBar(context));
+
+        return dialog.show();
+    }
 }
